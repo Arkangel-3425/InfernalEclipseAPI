d07c7ebf497f7687bf4df714611b53e36af128f5
Arkangel-3425 <arkangel3425@gmail.com>
2026-09-09 23:34:18 -0600

    added Seismic Station and Frost artifact to confused zombie loot pool

---
+++ Common/Globals/GlobalNPCs/LootAdjustments/ThoriumShopAdjustments.cs
@@ -0,0 +1,27 @@
+﻿using SOTS;
+
+public class ThoriumShopAdjustments : GlobalNPC
+{
+    public override void ModifyShop(NPCShop shop)
+    {
+        if (ModContent.TryFind<ModNPC>("ThoriumMod", "ConfusedZombie", out ModNPC confusedZombie))
+        {
+            if (shop.NpcType == confusedZombie.Type)
+            {
+                // Excavator Summon Station
+                if (ModContent.TryFind<ModItem>("SOTS", "SeismicStation", out ModItem excavatorStation))
+                {
+                    Condition downedExcavator = new Condition("Mods.IEoR.Conditions.DownedExcavator", () => SOTSWorld.downedExcavator);
+                    shop.Add(excavatorStation.Type, downedExcavator); //Boss cant be fought outside Arena need to fix -Arkangel
+                }
+
+                // Polaris Summon Station
+                if (ModContent.TryFind<ModItem>("SOTS", "FrostArtifact", out ModItem polarisStation))
+                {
+                    Condition downedPolaris = new Condition("Mods.IEoR.Conditions.DownedPolaris", () => SOTSWorld.downedAmalgamation);
+                    shop.Add(polarisStation.Type, downedPolaris); //works fine no notes -Arkangel
+                }
+            }
+        }
+    }
+}
